5f9f8580b99e7d42de50491b19099acfb612fdde
shuhei takamatsu <takamatushuhei@outlook.jp>
2019-03-17 13:42:31 +0900

    add .bashrc git main

---
+++ git_alias.bash
@@ -0,0 +1,8 @@
+#git alias
+alias ft='git fetch'
+alias st='git status'
+alias cm='git commit -m'
+alias sgl='git log --oneline --graph --all --decorate'
+alias add='git add -A'
+alias sbup='git submodule update --init --recursive'
+#echo git finished
---
+++ main.bash
@@ -0,0 +1,5 @@
+#くだらないのから分類しにくいのまで
+alias sl='sl -F -e -a'
+alias LS='LS -e'
+alias vim='nvim'
+#echo main finished
